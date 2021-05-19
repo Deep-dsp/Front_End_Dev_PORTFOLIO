d5b12431cc3371e0a471eb147072680eb5e39de7
Deepkumar Patel <dspguru97@gmail.com>
2021-05-18 22:03:34 -0400

    type-writer-effect-done

---
+++ js/components/typeWriter.js
@@ -0,0 +1,16 @@
+const position = ["Front End Web Developer", "Media Designer", "3D Artist"];
+
+let cursor = gsap.to('p.position .cursor', {
+    opacity:0,
+    ease: "power2.inOut",
+    repeat: -1
+});
+
+let masterTl = gsap.timeline( {repeat: -1} );
+
+position.forEach( word => {
+
+    let tl = gsap.timeline({ repeat:1, yoyo: true, repeatDelay: 1});
+    tl.to('.position',{ duration: 1, text: word});
+    masterTl.add(tl);
+});
